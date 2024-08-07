3d79b6982aecb67f829a14cdbe14fec7b6bbb532
Edward Lopez <edtolo92@gmail.com>
2024-08-07 14:03:55 -0700

    first landing page

---
+++ app.js
@@ -0,0 +1,8 @@
+const body = document.body;
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+button.addEventListener("click", () => {
+  body.style.background =
+    'url("https://images.unsplash.com/photo-1660659236367-710aa4ae7e19?q=80&w=1635&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D") center center / cover';
+});
